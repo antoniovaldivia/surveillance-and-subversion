685de6c3e26d72817f4e426d45081ca1624cace0
Antonio Valdivia <antoniovaldivia@gmail.com>
2020-04-10 20:01:54 -0400

    Some images

--- js/index.js
+++ js/index.js
@@ -86,3 +86,37 @@ if (amtScrolled > h1Top + h1Height) {
     window.addEventListener('load', theStateOfThings)
     window.addEventListener('scroll', theStateOfThings)
     window.addEventListener('resize', theStateOfThings)
+
+
+    const loadContentFromHtmlFile = (url) => {
+
+        // 1. Put up some type of UI to inform the user
+    
+        // 2. Fetch the file specified as url
+        fetch(url)
+            .then((response) => {
+                // Converts the file to text
+                return response.text()
+            })
+            .then((html) => {
+                // 3. Parse the document into text/html
+                let parser = new DOMParser()
+                let newDocument = parser.parseFromString(html, 'text/html')
+    
+                // 4. Pull out the new #content
+                let $oldContent = document.querySelector('#content')
+                let $newContent = newDocument.querySelector('#content')
+    
+                // 5. Replace the old content with the new content
+                $oldContent.innerHTML += $newContent.innerHTML
+                
+                // 6. Clear the UI loading/etc
+    
+    
+            })
+    
+    }
+    
+    
+    
+loadContentFromHtmlFile('about.html')
