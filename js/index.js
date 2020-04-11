7f199cd59395850a653d20d665e857226d955e81
Antonio Valdivia <antoniovaldivia@gmail.com>
2020-04-10 22:52:03 -0400

    Improved

--- js/index.js
+++ js/index.js
@@ -6,117 +6,89 @@ let $titleBar = document.querySelector('.titleBar')
 let $yearBar = document.querySelector('.yearBar')
 let title = document.querySelector('h1').textContent
 
-
 window.addEventListener('scroll', event => {
-let windowH = window.innerHeight
-let documentH = document.documentElement.scrollHeight
-let amtScrolled = window.scrollY
-let ttlAvailable = documentH - windowH
-let percent = amtScrolled / ttlAvailable
+    let windowH = window.innerHeight
+    let documentH = document.documentElement.scrollHeight
+    let amtScrolled = window.scrollY
+    let ttlAvailable = documentH - windowH
+    let percent = amtScrolled / ttlAvailable
 
-$bar.style.width = `${percent * 100}%`
-$progressText.textContent = `${Math.round(percent*100)}%`
+    $bar.style.width = `${percent * 100}%`
+    $progressText.textContent = `${Math.round(percent * 100)}%`
 
-let h1Top = $h1.offsetTop
-let h1Height = $h1.clientHeight
-console.log(h1Top, h1Height, amtScrolled)
-console.log(year)
+    let h1Top = $h1.offsetTop
+    let h1Height = $h1.clientHeight
+    console.log(h1Top, h1Height, amtScrolled)
+    console.log(year)
 
 
-if (amtScrolled > h1Top + h1Height) {
-    console.log('H1 is off the top')
-    $titleBar.classList.add(`show`)
-    $yearBar.classList.add(`show`)
-    $titleBar.innerHTML = `${title}`;
-    $yearBar.innerHTML = `${year}`;
+    if (amtScrolled > h1Top + h1Height) {
+        console.log('H1 is off the top')
+        $titleBar.classList.add(`show`)
+        $yearBar.classList.add(`show`)
+        $titleBar.innerHTML = `${title}`;
+        $yearBar.innerHTML = `${year}`;
 
-} else {
-    $titleBar.classList.remove(`show`)
-    $yearBar.classList.remove(`show`)
-    $titleBar.innerHTML = ``;
-}
+    } else {
+        $titleBar.classList.remove(`show`)
+        $yearBar.classList.remove(`show`)
+        $titleBar.innerHTML = ``;
+    }
 
 })
 
 
-// let theStateOfThings = function () {
-    let theStateOfThings = () => {
+let theStateOfThings = () => {
 
-        let winH = document.documentElement.clientHeight
-        let winW = document.documentElement.clientWidth
-        let docH = document.documentElement.scrollHeight
-        let docW = document.documentElement.scrollWidth
-        let winY = window.scrollY
-        let winX = window.scrollX
-        let maxY = docH - winH
-        let maxX = docW - winW
-        let pctY = Math.round(winY / Math.max(maxY, 1) * 100)
-        let pctX = Math.round(winX / Math.max(maxX, 1) * 100)
-    
-    
-        let infoForElement = ''	
-    
-        let checkOneSection = ($sec) => {
-            let fromD = $sec.offsetTop
-            let top = $sec.getBoundingClientRect().top
-            let bottom = $sec.getBoundingClientRect().bottom
-            
-            let theId = $sec.getAttribute('id')
-    
-            if (top < winH && bottom > 0) {
-                infoForElement += `<li>${theId}</li>`
-            } else {
-                infoForElement += `<li><a href="#${theId}">${theId}</a></li>`
-            }
+    let winH = document.documentElement.clientHeight
+    let winW = document.documentElement.clientWidth
+    let docH = document.documentElement.scrollHeight
+    let docW = document.documentElement.scrollWidth
+    let winY = window.scrollY
+    let winX = window.scrollX
+    let maxY = docH - winH
+    let maxX = docW - winW
+    let pctY = Math.round(winY / Math.max(maxY, 1) * 100)
+    let pctX = Math.round(winX / Math.max(maxX, 1) * 100)
+    let infoForElement = ''
+    let checkOneSection = ($sec) => {
+        let fromD = $sec.offsetTop
+        let top = $sec.getBoundingClientRect().top
+        let bottom = $sec.getBoundingClientRect().bottom
+        let theId = $sec.getAttribute('id')
+        let thefirstChild = $sec.firstElementChild
+        let thelastChild = $sec.lastElementChild
+        if (top < winH && bottom > 0) {
+            infoForElement += `<li>${theId}</li>`
+            thefirstChild.classList.add(`aqua`)
+            thelastChild.classList.add(`pink`)
+        } else {
+            infoForElement += `<li><a href="#${theId}">${theId}</a></li>`
         }
-    
-        // Iterate through each matching eleemnt, call the checkOneSection function for each one
-        document.querySelectorAll('.yeearClass').forEach(checkOneSection)
-        
-    
-        $yearBar.innerHTML = `            
-
+    }
+    document.querySelectorAll('.yearClass').forEach(checkOneSection)
+    $yearBar.innerHTML = `            
         <ol class="nav">${infoForElement}</ol>
-
          `;
+}
 
-    }
-    
-    // 1                       2     3
-    window.addEventListener('load', theStateOfThings)
-    window.addEventListener('scroll', theStateOfThings)
-    window.addEventListener('resize', theStateOfThings)
+const loadContentFromHtmlFile = (url) => {
+    fetch(url)
+        .then((response) => {
+            // Converts the file to text
+            return response.text()
+        })
+        .then((html) => {
+            let parser = new DOMParser()
+            let newDocument = parser.parseFromString(html, 'text/html')
+            let $oldContent = document.querySelector('#infinite')
+            let $newContent = newDocument.querySelector('#infinite')
+            $oldContent.innerHTML += $newContent.innerHTML
+        })
 
+}
 
-    const loadContentFromHtmlFile = (url) => {
-
-        // 1. Put up some type of UI to inform the user
-    
-        // 2. Fetch the file specified as url
-        fetch(url)
-            .then((response) => {
-                // Converts the file to text
-                return response.text()
-            })
-            .then((html) => {
-                // 3. Parse the document into text/html
-                let parser = new DOMParser()
-                let newDocument = parser.parseFromString(html, 'text/html')
-    
-                // 4. Pull out the new #content
-                let $oldContent = document.querySelector('#content')
-                let $newContent = newDocument.querySelector('#content')
-    
-                // 5. Replace the old content with the new content
-                $oldContent.innerHTML += $newContent.innerHTML
-                
-                // 6. Clear the UI loading/etc
-    
-    
-            })
-    
-    }
-    
-    
-    
+window.addEventListener('load', theStateOfThings)
+window.addEventListener('scroll', theStateOfThings)
+window.addEventListener('resize', theStateOfThings)
 loadContentFromHtmlFile('about.html')
